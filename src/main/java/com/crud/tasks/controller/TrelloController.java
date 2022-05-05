b0cb96934401cccfb223f6ce0bd6f2646900c864
Magdalena Wisniewska <magda.zz@hotmail.com>
2022-05-05 12:03:12 +0200

    22.3 Ułożenie kodu (Rest Template)

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,29 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/v1/trello")
+@RequiredArgsConstructor
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("getTrelloBoards")
+    public List<TrelloBoardDto> getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+        return trelloBoards.stream()
+                .filter(board -> board.getName().contains("Kodilla") && !board.getName().isEmpty() && !board.getId().isEmpty())
+                .collect(Collectors.toList());
+
+    }
+}
