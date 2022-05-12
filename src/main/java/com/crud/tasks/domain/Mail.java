fad47d65bf3770067f2f2a660dfe0d5237ecae68
Magdalena Wisniewska <magda.zz@hotmail.com>
2022-05-12 09:51:34 +0200

    23.3 Wysyłanie maili z aplikacji

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,18 @@
+package com.crud.tasks.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String toCc;
+
+}
+
